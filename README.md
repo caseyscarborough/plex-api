0313f1d349111ae445cb9bde4053348b0584d8ce
Casey Scarborough <caseyscarborough@gmail.com>
2023-08-14 17:48:04 -0400

    Update examples

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # plex-api
-A Java Library for interacting with the Plex Media Server API.
+A fluent Java Library for interacting with the Plex Media Server API.
 
 ## Adding the Dependency
 
@@ -17,4 +17,83 @@ dependencies {
 
 ## Examples
 
-Examples can be found [here](https://github.com/caseyscarborough/plex-api/blob/master/examples/src/main/java/plex/api/examples/Application.java).
+```java
+// Create a new server. Host defaults to http://localhost:32400.
+PlexServer server = new PlexServer("http://localhost:32400", "<plex token>");
+
+// Get the server name
+server.host();
+
+// Get the current user's username
+server.myPlexUsername();
+
+// Get the server library
+Library library = server.library();
+
+// List all sections
+library.sections().forEach(System.out::println);
+
+// List all settings
+server.settings().all().forEach(System.out::println);
+
+// Get a section/movie/show by ID
+library.sectionById("4");
+library.movieById("112486");
+library.showById("59816");
+
+// Get a section by name
+Section tv = library.section("TV Shows");
+Section movies = library.section("Movies");
+
+// Scan a section for new media
+movies.scan();
+
+// List all items for a section
+tv.all().forEach(System.out::println);
+
+// Get a movie by title
+Movie movie = movies.movie("The Lord of the Rings: The Fellowship of the Ring");
+System.out.println(movie);
+
+// Get a show by title
+Show show = tv.show("The Mandalorian");
+System.out.println(show);
+
+// Get all episodes for a show (method #1)
+show
+    .seasons()
+    .stream()
+    .flatMap(s -> s.episodes().stream())
+    .forEach(System.out::println);
+
+// Get all episodes for a show (method #2, more efficient than going through seasons)
+show
+    .allEpisodes()
+    .forEach(System.out::println);
+
+// Find a movie using a filter
+movies
+    .find((v) -> v.title().contains("Lord of the Rings"))
+    .forEach(System.out::println);
+
+// Edit a movie
+movie.edit(
+    // Pass the key-value pairs in one after another
+    "summary.value", "Young hobbit Frodo Baggins, after inheriting a mysterious ring from his uncle Bilbo, must leave his home in order to keep it from falling into the hands of its evil creator. Along the way, a fellowship is formed to protect the ringbearer and make sure that the ring arrives at its final destination: Mt. Doom, the only place where it can be destroyed.",
+    "summary.locked", "0",
+    "title.value", "The Lord of the Rings: The Fellowship of the Ring",
+    "title.locked", "0"
+);
+
+// Upload poster from a URL
+movie.uploadPosterFromUrl("https://www.themoviedb.org/t/p/w1280/6oom5QYQ2yQTMJIbnvbkBL9cHo6.jpg");
+
+// Upload poster from a file
+movie.uploadPosterFromFile("lotr.jpg");
+
+// Retrieve/download a poster
+byte[] poster = movie.poster();
+Files.write(Paths.get("lotr.jpg"), poster);
+```
+
+Examples can also be found [here](https://github.com/caseyscarborough/plex-api/blob/master/examples/src/main/java/plex/api/examples/Application.java).
--- examples/src/main/java/plex/api/examples/Application.java
+++ examples/src/main/java/plex/api/examples/Application.java
@@ -1,91 +1,92 @@
 package plex.api.examples;
 
+import plex.api.Library;
 import plex.api.Movie;
 import plex.api.PlexServer;
+import plex.api.Section;
 import plex.api.Show;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         final String token = System.getenv("PLEX_TOKEN");
         final String host = System.getenv("PLEX_SERVER");
 
         // Create a new server. Host defaults to http://localhost:32400.
-        PlexServer server = new PlexServer(host, token);
+        final PlexServer server = new PlexServer(host, token);
         System.out.println("Server Host: " + server.host());
         System.out.println("Plex Username: " + server.myPlexUsername());
 
         // Get the server library
-        server.library();
+        final Library library = server.library();
 
         // List all sections
-        server.library().sections().forEach(System.out::println);
+        library.sections().forEach(System.out::println);
 
         // List all settings
         server.settings().all().forEach(System.out::println);
 
+        // Get a section/movie/show by ID
+        library.sectionById("4");
+        library.movieById("112486");
+        library.showById("59816");
+
+        // Get a section by name
+        final Section tv = library.section("TV Shows");
+        final Section movies = library.section("Movies");
+
+        // Scan a section for new media
+        movies.scan();
+
         // List all items for a section
-        server.library().section("TV Shows").all().forEach(System.out::println);
+        tv.all().forEach(System.out::println);
 
         // Get a movie by title
-        Movie lotr = server.library()
-            .section("Movies")
-            .movie("The Lord of the Rings: The Fellowship of the Ring");
-        System.out.println(lotr);
+        final Movie movie = movies.movie("The Lord of the Rings: The Fellowship of the Ring");
+        System.out.println(movie);
 
         // Get a show by title
-        Show show = server.library()
-            .section("TV Shows")
-            .show("Rick and Morty");
+        final Show show = tv.show("The Mandalorian");
         System.out.println(show);
 
         // Get all episodes for a show (method #1)
-        server.library()
-            .section("TV Shows")
-            .show("Rick and Morty")
+        show
             .seasons()
             .stream()
             .flatMap(s -> s.episodes().stream())
             .forEach(System.out::println);
 
         // Get all episodes for a show (method #2, more efficient than going through seasons)
-        server.library()
-            .section("TV Shows")
-            .show("Rick and Morty")
+        show
             .allEpisodes()
             .forEach(System.out::println);
 
         // Find a movie using a filter
-        server.library()
-            .section("Movies")
+        movies
             .find((v) -> v.title().contains("Lord of the Rings"))
             .forEach(System.out::println);
 
         // Edit a movie
-        server.library()
-            .section("Movies")
-            .movie("The Lord of the Rings: The Fellowship of the Ring")
-            .edit(
-                // Pass the key-value pairs in one after another
-                "summary.value", "Young hobbit Frodo Baggins, after inheriting a mysterious ring from his uncle Bilbo, must leave his home in order to keep it from falling into the hands of its evil creator. Along the way, a fellowship is formed to protect the ringbearer and make sure that the ring arrives at its final destination: Mt. Doom, the only place where it can be destroyed.",
-                "summary.locked", "0",
-                "title.value", "The Lord of the Rings: The Fellowship of the Ring",
-                "title.locked", "0"
-            );
-
-        // Scan a section for new media
-        server.library().section("4K Movies").scan();
+        movie.edit(
+            // Pass the key-value pairs in one after another
+            "summary.value", "Young hobbit Frodo Baggins, after inheriting a mysterious ring from his uncle Bilbo, must leave his home in order to keep it from falling into the hands of its evil creator. Along the way, a fellowship is formed to protect the ringbearer and make sure that the ring arrives at its final destination: Mt. Doom, the only place where it can be destroyed.",
+            "summary.locked", "0",
+            "title.value", "The Lord of the Rings: The Fellowship of the Ring",
+            "title.locked", "0"
+        );
 
         // Upload poster from a URL
-        server.library()
-            .section("Movies")
-            .movie("The Lord of the Rings: The Fellowship of the Ring")
-            .uploadPosterFromUrl("https://www.themoviedb.org/t/p/w1280/6oom5QYQ2yQTMJIbnvbkBL9cHo6.jpg");
+        movie.uploadPosterFromUrl("https://www.themoviedb.org/t/p/w1280/6oom5QYQ2yQTMJIbnvbkBL9cHo6.jpg");
 
         // Upload poster from a file
-        server.library()
-            .section("Movies")
-            .movie("The Lord of the Rings: The Fellowship of the Ring")
-            .uploadPosterFromFile("/Users/casey/Downloads/lotr.jpg");
+        movie.uploadPosterFromFile("lotr.jpg");
+
+        // Retrieve/download a poster
+        byte[] poster = movie.poster();
+        Files.write(Paths.get("lotr.jpg"), poster);
     }
 }
